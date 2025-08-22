9d322cb90b318144fb73a6d1a1ca555f599f432b
Priyanshu Ranjan <127554861+priyanshuranjan16@users.noreply.github.com>
2025-08-22 22:27:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
+  title: "Tutorly",
   description: "Real-time AI Teaching Platform",
 };
 
